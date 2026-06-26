87fe3066578c8ac055a036491b79afacdc7eb6b4
Alex.A.Om.temp <Alex.A.Om.temp@gmail.com>
2026-06-26 15:53:04 +0300

    readme fix

--- Assets/Custom/Scripts/FogMovement.cs
+++ Assets/Custom/Scripts/FogMovement.cs
@@ -8,18 +8,22 @@ namespace Custom
         [SerializeField] private RectTransform _startMarker;
         [SerializeField] private RectTransform _finishMarker;
         [SerializeField] private float _speed = 10f;
+        
         private RectTransform _rectTransform;
+        private float _direction;
+        
         private void Awake()
         {
             _rectTransform = GetComponent<RectTransform>();
+            _direction = _startMarker.anchoredPosition.x <= _finishMarker.anchoredPosition.x ? 1f : -1f;
         }
 
         private void Update()
         {
             var position = _rectTransform.anchoredPosition;
-            _rectTransform.anchoredPosition = new Vector2(position.x + Time.deltaTime * _speed, position.y);
+            _rectTransform.anchoredPosition = new Vector2(position.x + Time.deltaTime * _speed * _direction, position.y);
          
-            if (  _rectTransform.anchoredPosition.x >= _finishMarker.anchoredPosition.x)
+            if ( Mathf.Abs(_rectTransform.anchoredPosition.x - _finishMarker.anchoredPosition.x) < 0.1f)
                 ToStartPos();
         }
 
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Kosinka
+# Classic Solitaire
 
-Простой пасьянс косынка. Только core-gameplay.
-
-
-![](C:\Users\Alex\AppData\Roaming\marktext\images\2026-03-02-19-59-47-image.png)
+![](Screenshots/Gameplay.PNG)
